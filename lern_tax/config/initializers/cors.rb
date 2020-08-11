2c8b7d9e11d5f05f50f7b2363b9386bdf38f663b
M. Brock Podgurski <mbpod10@gmail.com>
2020-08-11 09:55:23 -0400

    first commit

---
+++ lern_tax/config/initializers/cors.rb
@@ -0,0 +1,23 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001'
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
+
+# WHEN YOU GO TO DEPLOY REACT APP! YOU NEED TO CHANGE DOMAIN
+
+#   Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins 'REACT DOMAIN HERE'
+  
+#       resource '*',
+#         headers: :any,
+#         methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#         credentials: true
+#     end
+#   end
