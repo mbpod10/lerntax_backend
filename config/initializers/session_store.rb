cd15dbfa6019621e9a55526eb9c70926c10c6ad3
M. Brock Podgurski <mbpod10@gmail.com>
2020-08-17 15:40:54 -0400

    post deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,18 @@
 
 if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, 
-    key: "_authentication_app", domain: "http://localhost:3000"
-    
-    else 
+    key: "_authentication_app", domain: "https://lern-tax-api.herokuapp.com"
+else
         Rails.application.config.session_store :cookie_store, 
         key: "_authentication_app"
     end
     
+
+    # if Rails.env == "production"
+    #     Rails.application.config.session_store :cookie_store, 
+    #     key: "_authentication_app", domain: "http://localhost:3000"
+    #     elsif 
+    #         Rails.application.config.session_store :cookie_store, 
+    #         key: "_authentication_app", domain: 
+    #     end
+        
