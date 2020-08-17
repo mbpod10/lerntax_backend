0008e8b7b8e6ade3232d9effb52c30098578998c
M. Brock Podgurski <mbpod10@gmail.com>
2020-08-17 14:34:59 -0400

    again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, 
-    key: "_authentication_app", domain: "http://localhost:3000", same_site: :none
+    key: "_authentication_app", domain: "http://localhost:3000"
     
     else 
         Rails.application.config.session_store :cookie_store, 
